6b77f133b94b92c0348b7d96bd8736f928204af1
rie <6197788+criennson@users.noreply.github.com>
2020-06-10 17:25:43 +0900

    Dynamic and Reusable Components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
-function Hi() {
-  return <div>Hello World</div>
+const Hi = ({ name }) => <div>Hello { name }!</div>;
+
+function MediaCard( props ) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <p>{props.body}</p>
+      <img src={props.imageUrl} />
+    </>
+  );
 }
 
-ReactDOM.render(<Hi/>, document.querySelector('#root'));
+const card = {
+  title:  'Title',
+  body: 'I hope you enjoy learning React!',
+  imageUrl: 'https://placekitten.com/g/64/64'
+};
+
+// ReactDOM.render(<Hi name= "Dave" />, document.querySelector('#root'));
+
+ReactDOM.render(
+  <MediaCard {...card}/>,
+   document.querySelector('#root')
+);
